8608a079e31adde0bbc3da18852912bbcc5eb5e0
Andy Ta <andyta97@ou.edu>
2020-11-30 06:11:42 -0600

    pokedex

---
+++ PokemonJSON/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokemonJSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isFrontViewActive = true;
+        PokeInfo Pokemon;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PokemonAPI pokemonApiResults;
+
+            string apiURL = @"https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1100";
+
+            using (var client = new HttpClient())
+            {
+                string jsonResults = client.GetStringAsync(apiURL).Result;
+
+                pokemonApiResults = JsonConvert.DeserializeObject<PokemonAPI>(jsonResults);
+            }
+            foreach (var item in pokemonApiResults.results)
+            {
+                nameComBox.Items.Add(item);
+            }
+        }
+
+        private void pokeInfoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var pokemonResult = (AllResults) nameComBox.SelectedItem;
+            string pokemonURL = pokemonResult.url;
+
+            pokeNameTxtBlck.Text = pokemonResult.name.ToUpper();
+            pokeNameTxtBlck.Visibility = Visibility.Visible;
+            pokeNameRct.Visibility = Visibility.Visible;
+            switchBtn.Visibility = Visibility.Visible;
+            statsRct.Visibility = Visibility.Visible;
+            pokeInfoTxtBlck.Visibility = Visibility.Visible;
+            pokeImg.Visibility = Visibility.Visible;
+
+            using (var client = new HttpClient())
+            {
+                string jsonResults = client.GetStringAsync(pokemonURL).Result;
+
+                Pokemon = JsonConvert.DeserializeObject<PokeInfo>(jsonResults);
+            }
+            
+            pokeImg.Source = new BitmapImage(new Uri(Pokemon.sprites.front_default));
+
+            pokeInfoTxtBlck.Text = $"STATS:\n\nHEIGHT - {Pokemon.height}\n\nWEIGHT - {Pokemon.weight}";
+        }
+
+        private void switchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isFrontViewActive == true)
+            {
+                isFrontViewActive = false;
+                pokeImg.Source = new BitmapImage(new Uri(Pokemon.sprites.back_default));
+            }
+            else
+            {
+                isFrontViewActive = true;
+                pokeImg.Source = new BitmapImage(new Uri(Pokemon.sprites.front_default));
+            }
+        }
+
+        private void nameComBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (nameComBox.SelectedItem != null)
+            {
+                pokeInfoBtn.IsEnabled = true;
+                switchBtn.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ PokemonJSON/PokemonAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonJSON
+{
+    class PokemonAPI
+    {
+        public List<AllResults> results { get; set; }
+    }
+
+    public class AllResults
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return CapitalizeFirstLetterOfName();
+        }
+        public string CapitalizeFirstLetterOfName()
+        {
+            return name[0].ToString().ToUpper() + name.Substring(1, name.Length - 1);
+        }
+    }
+
+    public class PokeInfo
+    {
+        public int weight { get; set; }
+        public int height { get; set; }
+        public Sprites sprites { get; set; }
+    }
+
+    public class Sprites
+    {
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+    }
+}
